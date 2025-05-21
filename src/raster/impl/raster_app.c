faa446759acf66619276614094294b27a61c234f
Mikael Deurell <mikael@deurell.net>
2025-05-21 08:26:44 +0200

    naming fix

--- src/raster/impl/raster_app.c
+++ src/raster/impl/raster_app.c
@@ -36,7 +36,7 @@ static struct
 
     // Quit flag
     bool should_quit;
-} app_state = { 0 };
+ } engine_state = { 0 };
 
 // Emscripten main loop function
 #ifdef __EMSCRIPTEN__
@@ -83,7 +83,7 @@ void emscripten_main_loop(void)
 // Expose window handle to input system
 GLFWwindow* _rapp_get_window(void)
 {
-    return app_state.window;
+    return engine_state.window;
 }
 
 bool rapp_init(const rapp_desc_t* desc)
@@ -102,16 +102,16 @@ bool rapp_init(const rapp_desc_t* desc)
 #ifdef __EMSCRIPTEN__
     // For WebGL, we need to use the ES profile
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);fds
 #else
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
     // Create window
-    app_state.window = glfwCreateWindow(desc->window.width, desc->window.height, desc->window.title, NULL, NULL);
+    engine_state.window = glfwCreateWindow(desc->window.width, desc->window.height, desc->window.title, NULL, NULL);
 
-    if (!app_state.window)
+    if (!engine_state.window)
     {
         rlog_fatal("Failed to create GLFW window\n");
         glfwTerminate();
@@ -119,53 +119,53 @@ bool rapp_init(const rapp_desc_t* desc)
     }
 
     // Make OpenGL context current
-    glfwMakeContextCurrent(app_state.window);
+    glfwMakeContextCurrent(engine_state.window);
 
     // Initialize graphics subsystem
     if (!rgfx_init())
     {
         rlog_fatal("Failed to initialize graphics system\n");
-        glfwDestroyWindow(app_state.window);
+        glfwDestroyWindow(engine_state.window);
         glfwTerminate();
         return false;
     }
 
     // Create main camera
-    app_state.main_camera = rgfx_camera_create(&desc->camera);
-    if (!app_state.main_camera)
+    engine_state.main_camera = rgfx_camera_create(&desc->camera);
+    if (!engine_state.main_camera)
     {
         rlog_fatal("Failed to create main camera\n");
         rgfx_shutdown();
         glfwTerminate();
         return false;
     }
-    rgfx_set_active_camera(app_state.main_camera);
+    rgfx_set_active_camera(engine_state.main_camera);
 
     // Set input callbacks
-    glfwSetKeyCallback(app_state.window, _rinput_key_callback);
-    glfwSetMouseButtonCallback(app_state.window, _rinput_mouse_button_callback);
-    glfwSetCharCallback(app_state.window, _rinput_char_callback); // Register char callback
+    glfwSetKeyCallback(engine_state.window, _rinput_key_callback);
+    glfwSetMouseButtonCallback(engine_state.window, _rinput_mouse_button_callback);
+    glfwSetCharCallback(engine_state.window, _rinput_char_callback); // Register char callback
 
     // Register framebuffer size callback
-    glfwSetFramebufferSizeCallback(app_state.window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(engine_state.window, framebuffer_size_callback);
 
     // Manually set the viewport initially
     int fb_width, fb_height;
-    glfwGetFramebufferSize(app_state.window, &fb_width, &fb_height);
+    glfwGetFramebufferSize(engine_state.window, &fb_width, &fb_height);
     glViewport(0, 0, fb_width, fb_height);
 
     // Initialize time
-    app_state.currentTime = glfwGetTime();
-    app_state.lastTime    = app_state.currentTime;
-    app_state.deltaTime   = 0.0f;
+    engine_state.currentTime = glfwGetTime();
+    engine_state.lastTime    = engine_state.currentTime;
+    engine_state.deltaTime   = 0.0f;
 
     // Store callbacks
-    app_state.update_callback  = desc->update_fn;
-    app_state.draw_callback    = desc->draw_fn;
-    app_state.cleanup_callback = desc->cleanup_fn;
+    engine_state.update_callback  = desc->update_fn;
+    engine_state.draw_callback    = desc->draw_fn;
+    engine_state.cleanup_callback = desc->cleanup_fn;
 
     // Initialize quit flag
-    app_state.should_quit = false;
+    engine_state.should_quit = false;
 
     return true;
 }
@@ -178,35 +178,35 @@ void rapp_run(void)
     // Note: cleanup is handled in the main loop function when the loop is cancelled
 #else
     // Standard main loop for desktop platforms
-    while (!glfwWindowShouldClose(app_state.window) && !app_state.should_quit)
+    while (!glfwWindowShouldClose(engine_state.window) && !engine_state.should_quit)
     {
         // Update time values
-        app_state.lastTime    = app_state.currentTime;
-        app_state.currentTime = glfwGetTime();
-        app_state.deltaTime   = app_state.currentTime - app_state.lastTime;
+        engine_state.lastTime    = engine_state.currentTime;
+        engine_state.currentTime = glfwGetTime();
+        engine_state.deltaTime   = engine_state.currentTime - engine_state.lastTime;
 
         _rinput_update();
         glfwPollEvents();
 
-        if (app_state.update_callback)
+        if (engine_state.update_callback)
         {
-            app_state.update_callback(app_state.deltaTime);
+            engine_state.update_callback(engine_state.deltaTime);
         }
 
-        if (app_state.draw_callback)
+        if (engine_state.draw_callback)
         {
-            app_state.draw_callback();
+            engine_state.draw_callback();
         }
 
-        if (app_state.window)
+        if (engine_state.window)
         {
-            glfwSwapBuffers(app_state.window);
+            glfwSwapBuffers(engine_state.window);
         }
     }
 
-    if (app_state.cleanup_callback)
+    if (engine_state.cleanup_callback)
     {
-        app_state.cleanup_callback();
+        engine_state.cleanup_callback();
     }
 
     rapp_shutdown();
@@ -215,30 +215,30 @@ void rapp_run(void)
 
 void rapp_quit(void)
 {
-    if (app_state.window)
+    if (engine_state.window)
     {
-        glfwSetWindowShouldClose(app_state.window, GLFW_TRUE);
+        glfwSetWindowShouldClose(engine_state.window, GLFW_TRUE);
     }
-    app_state.should_quit = true;
+    engine_state.should_quit = true;
 }
 
 void rapp_shutdown(void)
 {
     // Cleanup camera
-    if (app_state.main_camera)
+    if (engine_state.main_camera)
     {
-        rgfx_camera_destroy(app_state.main_camera);
-        app_state.main_camera = NULL;
+        rgfx_camera_destroy(engine_state.main_camera);
+        engine_state.main_camera = NULL;
     }
 
     // Shutdown graphics subsystem
     rgfx_shutdown();
 
     // Destroy window and terminate GLFW
-    if (app_state.window)
+    if (engine_state.window)
     {
-        glfwDestroyWindow(app_state.window);
-        app_state.window = NULL;
+        glfwDestroyWindow(engine_state.window);
+        engine_state.window = NULL;
     }
 #ifndef __EMSCRIPTEN__
     glfwTerminate();
@@ -247,24 +247,24 @@ void rapp_shutdown(void)
 
 float rapp_get_time(void)
 {
-    return app_state.currentTime;
+    return engine_state.currentTime;
 }
 
 float rapp_get_delta_time(void)
 {
-    return app_state.deltaTime;
+    return engine_state.deltaTime;
 }
 
 rgfx_camera_t* rapp_get_main_camera(void)
 {
-    return app_state.main_camera;
+    return engine_state.main_camera;
 }
 
 void rapp_get_window_size(int* width, int* height)
 {
-    if (app_state.window)
+    if (engine_state.window)
     {
-        glfwGetWindowSize(app_state.window, width, height);
+        glfwGetWindowSize(engine_state.window, width, height);
     }
     else if (width && height)
     {
